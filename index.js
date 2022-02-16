fe4730593051ec8002904e41083fec87f7062440
Wolftail8192 <4420299@gmail.com>
2022-02-16 10:46:00 +0200

    les1-1.2

--- index.js
+++ index.js
@@ -16,10 +16,15 @@ fs.mkdir(path.join(__dirname, 'main/inPerson'), (err)=>{
         console.log(err);
     }
 })
+
 fs.mkdir(path.join(__dirname, 'main/online'), (err)=>{
     if (err){
         console.log(err);
     }
 })
+fs.writeFileSync(path.join(__dirname,'inPerson','inPerson.txt'),'name')
 
+
+// let onlineUsers = ['name', 'age', 'city'];
+// let inPersonUsers = [{ name: "Andrii", age: 22, city: "Lviv" }]
 
