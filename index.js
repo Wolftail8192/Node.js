5944495f208bcc80711ea01d0e46ffdb65433af7
Wolftail8192 <4420299@gmail.com>
2022-02-16 09:47:56 +0200

    les1-1.2

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const  helper = require('./helper')
 
 helper.greeting('Evgeniy');
+
+
+const fs = require('fs')
+const path = require("path");
+
+fs.mkdir(path.join(__dirname, 'main'), (err)=>{
+    if (err){
+        console.log(err);
+    }
+})
+fs.mkdir(path.join(__dirname, 'main/inPerson'), (err)=>{
+    if (err){
+        console.log(err);
+    }
+})
+fs.mkdir(path.join(__dirname, 'main/online'), (err)=>{
+    if (err){
+        console.log(err);
+    }
+})
+
+
