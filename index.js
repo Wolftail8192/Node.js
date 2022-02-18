46eb9a93d21313fb00fd16d0c9ba622e06d25d88
Wolftail8192 <4420299@gmail.com>
2022-02-18 05:38:37 +0200

    les1-2.0

--- index.js
+++ index.js
@@ -23,14 +23,59 @@ fs.mkdir(path.join(__dirname, 'main/online'), (err)=>{
         console.log(err);
     }
 })
-fs.writeFileSync(path.join(__dirname,'main','inPerson','inPerson.txt'),'data',{})
-fs.writeFileSync(path.join(__dirname,'main','online','online.txt'),'data',{})
+fs.writeFileSync(path.join(__dirname,'main','inPerson','inPerson.txt'),'' )
+fs.writeFileSync(path.join(__dirname,'main','online','online.txt'),'')
 
 
-let onlineUsers = ['name', 'age', 'city'];
-data = onlineUsers;
-// let inPersonUsers = [{ name: "Andrii", age: 22, city: "Lviv" }]
+const onlineUsers = [
+    {name: 'Amadey', age: 22, city: 'Toronto'},
+    {name: 'Duncan', age: 652, city: 'NY'},
+    {name: 'Lucas', age: 18, city: 'Chicago'},
+];
 
-// JSON.stringify(onlineUsers)
+const inPersonUsers = [
+    {name: 'Marta', age: 26, city: 'Lviv'},
+    {name: 'Bogdan', age: 32, city: 'Lviv'},
+    {name: 'Kira', age: 20, city: 'Ternopil'},
+];
 
+onlineUsers.forEach(user => {
+    fs.writeFile(path.join(__dirname, 'main', 'online', `${user.name}`),
+        `${JSON.stringify(user, null, `\t`)}`, (err => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        }));
+});
 
+inPersonUsers.forEach(user => {
+    fs.writeFile(path.join(__dirname, 'main', 'inPerson', `${user.name}`),
+        `${JSON.stringify(user, null, `\t`)}`, (err => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        }));
+});
+
+function changeUsersFilesDir(prevFilesDir, nextFilesDir) {
+    fs.readdir(path.join(__dirname, 'main', `${prevFilesDir}`), (err, files) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        files.forEach(file => {
+            fs.rename(path.join(__dirname, 'main', `${prevFilesDir}`, `${file}`),
+                path.join(__dirname, 'main', `${nextFilesDir}`, `${file}`), (err => {
+                    if (err) {
+                        console.log(err);
+                        throw err;
+                    }
+                }));
+        });
+    });
+}
+
+changeUsersFilesDir('inPerson', 'online');
+changeUsersFilesDir('online', 'inPerson');
