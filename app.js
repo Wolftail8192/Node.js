73eb2d2f5e5bccde0a52bb1683fc32d2b154352e
Wolftail8192 <4420299@gmail.com>
2022-02-20 10:42:17 +0200

    les2_hw_1.3

--- app.js
+++ app.js
@@ -27,11 +27,37 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout:false}));
 app.set('views', path.join(__dirname, 'static'));
 
+const users = [];
+let error = '';
+
 app.get('/login', (req,res)=>{
     res.render('login')
 })
 
+app.post('/login', ({body},res)=>{
+    const userExist = users.some(user=>user.email === body.email)
+    if (userExist){
+        error = 'User with this email exist';
+        res.redirect('/error');
+        return;
+    }
+
+    users.push({...body, id: users.length ? users[users.length-1].id +1:1});
+    res.redirect('/users');
+});
+
+app.get('/users', (req,res)=>{
+    res.render('users', {users});
+});
+
+app.get('/error', (req,res)=>{
+    res.render('error', {error});
+});
+
+app.use((req,res)=>{
+    res.render('notFound');
+});
 
 app.listen(5100, () =>{
-    console.log('Server has started on port 5100')
+    console.log('Server has started on port 5100');
 })
