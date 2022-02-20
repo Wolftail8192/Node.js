1f93894aa29593cd13739a93c5666b310ef1501a
Wolftail8192 <4420299@gmail.com>
2022-02-20 09:30:39 +0200

    les2_hw_1.0

--- app.js
+++ app.js
@@ -13,3 +13,20 @@
 //
 // 4. зробити якщо не відпрацюють ендпоінти то на сторінку notFound редірект
 
+const express = require('express');
+const {engine} = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout:false}));
+app.set('views', path.join(__dirname, 'static'));
+
+app.listen(5100, () =>{
+    console.log('Server has started on port 5100')
+})
