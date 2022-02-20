347048db57471528910b125953d93353c3c24579
Wolftail8192 <4420299@gmail.com>
2022-02-20 09:51:31 +0200

    les2_hw_1.2

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout:false}));
 app.set('views', path.join(__dirname, 'static'));
 
+app.get('/login', (req,res)=>{
+    res.render('login')
+})
+
+
 app.listen(5100, () =>{
     console.log('Server has started on port 5100')
 })
